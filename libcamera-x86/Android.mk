f6bcaae09d0723b58e29f14e31869e0784266e0d
zombah <zombah@gmail.com>
2012-09-08 21:48:47 +0400

    Move libcamera from android-x86 to another subfolder
    Signed-off-by: zombah <zombah@gmail.com>

---
+++ libcamera-x86/Android.mk
@@ -0,0 +1,35 @@
+ifeq ($(USE_CAMERA_STUB),false)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -fno-short-enums -DHAVE_CONFIG_H
+
+LOCAL_C_INCLUDES := \
+	external/jpeg
+
+LOCAL_SRC_FILES := \
+	CameraFactory.cpp \
+	CameraHal.cpp \
+	CameraHardware.cpp \
+	Converter.cpp \
+	SurfaceDesc.cpp \
+	SurfaceSize.cpp \
+	Utils.cpp \
+	V4L2Camera.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+	libcamera_client \
+	libcutils \
+	libjpeg \
+	liblog \
+	libui \
+	libutils \
+
+LOCAL_MODULE := camera.default
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
