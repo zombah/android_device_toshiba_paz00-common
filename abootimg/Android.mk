8feffc971b960f81ccc327c401ba86bfc0f1c249
zombah <zombah@gmail.com>
2012-09-20 02:08:36 +0400

    Add abootimg sources
    Signed-off-by: zombah <zombah@gmail.com>

---
+++ abootimg/Android.mk
@@ -0,0 +1,16 @@
+ifeq  ($(strip $(BUILD_INCLUDE_ABOOTIMG)),true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := abootimg.c
+LOCAL_CFLAGS := -O3 -Wall -DHAS_BLKID
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libext2_blkid
+#LOCAL_STATIC_LIBRARIES	:= libc libext2_blkid
+LOCAL_C_INCLUDES := external/kernel-headers/original external/e2fsprogs/lib
+LOCAL_MODULE := abootimg
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+endif
