e67b49d8f7c6fcc31e3e33260c311fc488d0e6eb
zombah <zombah@gmail.com>
2013-09-28 02:41:34 +0400

    Temparary switch wext as nl80211 not working now
    Signed-off-by: zombah <zombah@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,39 +43,16 @@ BOARD_HAS_NO_SELECT_BUTTON              := true
 BOARD_HAS_LARGE_FILESYSTEM              := true
 #TARGET_RECOVERY_INITRC 			:= device/toshiba/paz00-common/prebuild/init.recovery.rc
 
-# Wifi related defines
-USES_TI_MAC80211 			:= false
-USES_BCMHD_NL80211			:= true
-USES_BCMHD_WEXT				:= false
-
-ifdef USES_TI_MAC80211
-BOARD_WPA_SUPPLICANT_DRIVER 		:= NL80211
-WPA_SUPPLICANT_VERSION 			:= VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB 	:= lib_driver_cmd_wl12xx
-BOARD_HOSTAPD_DRIVER 			:= NL80211
-BOARD_HOSTAPD_PRIVATE_LIB 		:= lib_driver_cmd_wl12xx
-BOARD_SOFTAP_DEVICE			:= wlan0
-COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
-endif
-
-ifdef USES_BCMHD_WEXT
+#BOARD_WPA_SUPPLICANT_DRIVER             := NL80211
 BOARD_WPA_SUPPLICANT_DRIVER             := WEXT
 WPA_SUPPLICANT_VERSION                  := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB        := private_lib_driver_cmd
+#BOARD_HOSTAPD_DRIVER                    := NL80211
 BOARD_HOSTAPD_DRIVER                    := WEXT
-BOARD_HOSTAPD_PRIVATE_LIB               := private_lib_driver_cmd
-endif
-
-ifdef USES_BCMHD_NL80211
-BOARD_WPA_SUPPLICANT_DRIVER             := NL80211
-WPA_SUPPLICANT_VERSION                  := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB        := private_lib_driver_cmd
-BOARD_HOSTAPD_DRIVER                    := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB               := private_lib_driver_cmd
 # Unknow options
 BOARD_LEGACY_NL80211_STA_EVENTS         := false
 BOARD_NO_APSME_ATTR			:= false
-endif
 
 # Wifi base
 BOARD_WLAN_DEVICE                       := wlan0
