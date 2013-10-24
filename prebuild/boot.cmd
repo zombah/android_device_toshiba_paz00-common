72db6679e0aa7432df8f2d3182cab9567851119e
zombah <zombah@gmail.com>
2013-10-24 17:58:34 +0400

    uboot: Update boot.cmd
    * Make kernel and ramdisk names more specific
    * Lower counter
    
    Signed-off-by: zombah <zombah@gmail.com>

--- prebuild/boot.cmd
+++ prebuild/boot.cmd
@@ -1,6 +1,6 @@
 echo === Toshiba AC100 Bootmenu ===
-setenv bootmenu_0 "Boot LNX  =mmc dev 0; ext2load mmc 0:2 0x1000000 /boot/zImage; ext2load mmc 0:2 0x2200000 /boot/initrd.gz; bootz 0x1000000 0x2200000;"
-setenv bootmenu_1 "Boot SOS  =mmc dev 0; ext2load mmc 0:1 0x1000000 /boot/zImage; ext2load mmc 0:1 0x2200000 /boot/initrd-recovery.gz; bootz 0x1000000 0x2200000;"
-setenv bootmenu_2 "Boot from SD =setenv bootargs console=tty0; mmc dev 1; ext2load mmc 1 0x1000000 /boot/zImage; ext2load mmc 1 0x2200000 /boot/initrd.gz; bootz 0x1000000 0x2200000;"
-setenv bootmenu_3 "Boot from USB=setenv bootargs $cmdline; usb dev 0; ext2load usb 0 0x1000000 /boot/zImage; ext2load usb 0 0x2200000 /boot/initrd.gz; bootz 0x1000000 0x2200000 0x2000000;"
-bootmenu 20
+setenv bootmenu_0 "Boot LNX CM-10.1 =ext2load mmc 0:2 0x1000000 /boot/zImage-boot-cm-10-1; ext2load mmc 0:2 0x2200000 /boot/initrd-boot-cm-10-1.gz; bootz 0x1000000 0x2200000;"
+setenv bootmenu_1 "Boot SOS CM-10.1 =ext2load mmc 0:1 0x1000000 /boot/zImage-recovery-cm-10-1; ext2load mmc 0:1 0x2200000 /boot/initrd-recovery-cm-10-1.gz; bootz 0x1000000 0x2200000;"
+setenv bootmenu_2 "Boot from SD fat=fatload mmc 1:1 0x1000000 /boot/zImage; fatload mmc 1:1 0x2200000 /boot/initrd.gz; bootz 0x1000000 0x2200000;"
+setenv bootmenu_3 "Boot from USB ext2=ext2load usb 0:1 0x1000000 /boot/zImage; ext2load usb 0:1 0x2200000 /boot/initrd.gz; bootz 0x1000000 0x2200000 0x2000000;"
+bootmenu 5
