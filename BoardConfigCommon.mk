a4c08a4e53e5a28b4dfac3e94e99585a8178c649
zombah <zombah@gmail.com>
2014-03-13 01:58:57 +0400

    Move MBM stuff out of device tree
    Signed-off-by: zombah <zombah@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,8 +149,6 @@ TARGET_ARCH_LOWMEM 			:= true
 
 # Build Huaweigeneric-ril
 BOARD_USES_HUAWEIGENERIC_RIL		:= false
-# Build Ericsson mbm-ril
-BOARD_USES_MBM_RIL			:= true
 
 # SELinux stuff
 HAVE_SELINUX 				:= true
