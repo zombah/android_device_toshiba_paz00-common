a48ffa70f6e22b4e1c17ae5d582e2a5ec4488610
zombah <zombah@gmail.com>
2014-11-19 02:58:34 +0300

    power: Switch to upstream Grouper power hal
    Signed-off-by: zombah <zombah@gmail.com>

--- power/power.c
+++ power/power.c
@@ -19,13 +19,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define LOG_TAG "Tegra PowerHAL"
+#define LOG_TAG "Grouper PowerHAL"
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-static void sysfs_write(const char *path,const char *s)
+#define BOOST_PATH      "/sys/devices/system/cpu/cpufreq/interactive/boost"
+static int boost_fd = -1;
+static int boost_warned;
+
+static void sysfs_write(char *path, char *s)
 {
     char buf[80];
     int len;
@@ -46,84 +50,55 @@ static void sysfs_write(const char *path,const char *s)
     close(fd);
 }
 
-static void sysfs_read(const char *path, char *s,int maxlen)
+static void grouper_power_init(struct power_module *module)
 {
-    char buf[80];
-    int len;
-    int fd = open(path, O_RDONLY);
-
-    if (fd < 0) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error opening %s: %s\n", path, buf);
-        return;
-    }
-
-    len = read(fd, s, maxlen);
-    if (len < 0) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error reading from %s: %s\n", path, buf);
-    }
-	
-	s[len]=0;
-
-    close(fd);
-}
-
-/* Maximum speed detected */
-static char max_speed[64];
-static int last_on = -1;
-
-static void tegra_power_init(struct power_module *module)
-{
-	/* init the vatiables */
-	strcpy(max_speed,"1000000");
-	
     /*
      * cpufreq interactive governor: timer 20ms, min sample 100ms,
+     * hispeed 700MHz at load 40%
      */
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
                 "20000");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
                 "30000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_maxspeed_load",
+    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
                 "85");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
 		"0");
+    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
+		"1");
 }
 
-static void tegra_power_set_interactive(struct power_module *module, int on)
+static void grouper_power_set_interactive(struct power_module *module, int on)
 {
-	/* Do not apply the same changes twice */
-	if (last_on == on)
-		return;
-
-	/* Save the original frequency if disabling screen */
-	if (!on) {
-		sysfs_read("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                max_speed, sizeof(max_speed) - 1);
-	}
-		
-	/*
-     * Lower maximum frequency to minimum when screen is off.  CPU 0 and 1 share a
+    /*
+     * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
      * cpufreq policy.
      */
+
     sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? max_speed : "216000");
+                on ? "1000000" : "500000");
+
+    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
+                on ? "1" : "0");
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/boost_factor",
                 on ? "0" : "2");
 
 }
 
-static void tegra_power_hint(struct power_module *module, power_hint_t hint,
+static void grouper_power_hint(struct power_module *module, power_hint_t hint,
                             void *data)
 {
+    char buf[80];
+    int len;
+
     switch (hint) {
     case POWER_HINT_VSYNC:
         break;
+
     default:
-		break;
+            break;
     }
 }
 
@@ -137,12 +112,12 @@ struct power_module HAL_MODULE_INFO_SYM = {
         .module_api_version = POWER_MODULE_API_VERSION_0_2,
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = POWER_HARDWARE_MODULE_ID,
-        .name = "Tegra Power HAL",
+        .name = "Grouper Power HAL",
         .author = "The Android Open Source Project",
         .methods = &power_module_methods,
     },
 
-    .init = tegra_power_init,
-    .setInteractive = tegra_power_set_interactive,
-    .powerHint = tegra_power_hint,
+    .init = grouper_power_init,
+    .setInteractive = grouper_power_set_interactive,
+    .powerHint = grouper_power_hint,
 };
